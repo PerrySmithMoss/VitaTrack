24c887a75e095ea68b15914376e29b8421bf1073
PerrySmithMoss <perrysmithmoss@gmail.com>
2023-01-10 08:37:57 +0000

    Fixed bug with env variable

--- client/graphql/fetcher.ts
+++ client/graphql/fetcher.ts
@@ -4,8 +4,7 @@ export function customFetcher<TData, TVariables>(
   headers?: RequestInit['headers'] | undefined
 ) {
   return async (): Promise<TData> => {
-    console.log(`SERVER_ENDPOINT: ${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}`)
-    const res = await fetch(`${process.env.SERVER_ENDPOINT}/graphql/`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}/graphql/`, {
       method: 'POST',
       ...{
         headers: {
